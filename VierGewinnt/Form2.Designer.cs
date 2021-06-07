d46bff6d59d0b12f6ea51c8b4714c8e6c4a74648
Philipp Kreuzer <kreuzerphilipp@bs-intra.de>
2021-06-07 13:54:11 +0300

    213

--- VierGewinnt/Form2.Designer.cs
+++ VierGewinnt/Form2.Designer.cs
@@ -32,6 +32,9 @@ namespace VierGewinnt
             this.button1 = new System.Windows.Forms.Button();
             this.lab1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_Up = new System.Windows.Forms.Button();
+            this.btn_down = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -63,11 +66,41 @@ namespace VierGewinnt
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(257, 155);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 173);
+            this.panel1.TabIndex = 3;
+            // 
+            // btn_Up
+            // 
+            this.btn_Up.Location = new System.Drawing.Point(583, 334);
+            this.btn_Up.Name = "btn_Up";
+            this.btn_Up.Size = new System.Drawing.Size(75, 23);
+            this.btn_Up.TabIndex = 4;
+            this.btn_Up.Text = "up";
+            this.btn_Up.UseVisualStyleBackColor = true;
+            this.btn_Up.Click += new System.EventHandler(this.btn_Up_Click);
+            // 
+            // btn_down
+            // 
+            this.btn_down.Location = new System.Drawing.Point(583, 363);
+            this.btn_down.Name = "btn_down";
+            this.btn_down.Size = new System.Drawing.Size(75, 23);
+            this.btn_down.TabIndex = 5;
+            this.btn_down.Text = "down";
+            this.btn_down.UseVisualStyleBackColor = true;
+            this.btn_down.Click += new System.EventHandler(this.btn_down_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_down);
+            this.Controls.Add(this.btn_Up);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.lab1);
             this.Controls.Add(this.button1);
@@ -83,5 +116,8 @@ namespace VierGewinnt
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label lab1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_Up;
+        private System.Windows.Forms.Button btn_down;
     }
 }
--- VierGewinnt/Form2.cs
+++ VierGewinnt/Form2.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
 namespace VierGewinnt
 {
     public partial class Form2 : Form
     {
+        private int X, Y;
         private Graphics g;
 
         public Form2(bool Fullscreen)
@@ -41,14 +41,40 @@ namespace VierGewinnt
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
-            
+            X = 100;
+            Y = 100;
+            Kreis(X, Y);
+        }
 
-            g.DrawEllipse(new Pen(Color.FromName("SlateBlue")), 10, 10, 10, 10);
+        
+        
+        private void btn_Up_Click(object sender, EventArgs e)
+        {
+            Y += 100;
+            Kreis(X, Y);
+        }
+
+        private void btn_down_Click(object sender, EventArgs e)
+        {
+
+            Y -= 100;
+            Kreis(X, Y);
+        }
+        private void  Kreis(int X, int Y)
+        {
+            
             g = this.CreateGraphics();
-            g.DrawLine(new Pen(new SolidBrush(Color.Black)),10, 10,10,10);
-            g.DrawEllipse(new Pen(new SolidBrush(Color.Black)), 1000, 1000, 30, 30);
+           
+            //g.DrawEllipse(new Pen(Color.FromName("Red")), 100, 100, 100, 100);
 
+            Color farbe = Color.Goldenrod;
+            using (SolidBrush pinsel = new SolidBrush(farbe))
+            {
+                g.FillEllipse((pinsel), X, Y, 100, 100);
+            }
+            
         }
     }
 }
+
+//http://www.bildungsgueter.de/CSharp/Pages/Beisp002Seite025.htm
