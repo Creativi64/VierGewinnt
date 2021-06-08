8761abe87e0dd1738dd36852588e45326294ae78
Philipp Kreuzer <kreuzerphilipp@bs-intra.de>
2021-06-08 06:31:35 +0300

    Last

--- VierGewinnt/Form2.cs
+++ VierGewinnt/Form2.cs
@@ -48,7 +48,7 @@ namespace VierGewinnt
             Y = 100;
             Kreis(X, Y);
 
-            g.DrawEllipse(new Pen(Color.Goldenrod, 20), 50 - 20, 50 - 20, 20 * 2, 20 * 2);
+            g.DrawEllipse(new Pen(Color.Goldenrod, 20), 50 - 45, 50 - 45, 45 * 2, 45 * 2);
         }
 
         private void btn_Up_Click(object sender, EventArgs e)
